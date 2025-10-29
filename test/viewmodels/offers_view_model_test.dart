0c9930feb49eaaf544b79c0e231a932c616b4b87
Ricardo da Silva Ogliari <rogliariping@gmail.com>
2025-10-28 23:06:38 -0300

    add unit tests to viewmodels

--- test/viewmodels/offers_view_model_test.dart
+++ test/viewmodels/offers_view_model_test.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:vence_ai/models/offer.dart';
 import 'package:vence_ai/repositories/location_repository.dart';
@@ -7,83 +9,104 @@ import 'package:vence_ai/repositories/offers_repository.dart';
 import 'package:vence_ai/utils/result.dart';
 import 'package:vence_ai/viewmodels/offers_view_model.dart';
 
-class MockOffersRepository extends Mock implements OffersRepository {}
+import 'offers_view_model_test.mocks.dart';
 
-class MockLocationRepository extends Mock implements LocationRepository {}
-
+@GenerateMocks([OffersRepository, LocationRepository])
 void main() {
   group('OffersViewmodel', () {
     late OffersViewmodel viewModel;
     late MockOffersRepository mockOffersRepository;
     late MockLocationRepository mockLocationRepository;
 
+    final offers = [
+      Offer(
+        currentPrice: 80,
+        originalPrice: 100,
+        store: 'Store A',
+        product: 'Product A',
+        latitude: -23.5505,
+        longitude: -46.6333,
+        image: '',
+      ),
+    ];
+    final position = Position(
+      latitude: -23.5505,
+      longitude: -46.6333,
+      timestamp: DateTime.now(),
+      accuracy: 0,
+      altitude: 0,
+      altitudeAccuracy: 0,
+      heading: 0,
+      headingAccuracy: 0,
+      speed: 0,
+      speedAccuracy: 0,
+    );
+
+    Result<List<Offer>> _dummyResultOffer() {
+      return Result.ok(offers);
+    }
+
+    Result<Position> _dummyResultPosition() {
+      return Result.ok(position);
+    }
+
     setUp(() {
       mockOffersRepository = MockOffersRepository();
       mockLocationRepository = MockLocationRepository();
     });
 
-    test(
-        'fetches offers and location, calculates distance and discount correctly',
-        () async {
-      final offers = [
-        Offer(
-          currentPrice: 80,
-          originalPrice: 100,
-          store: 'Store A',
-          product: 'Product A',
-          latitude: -23.5505,
-          longitude: -46.6333,
-          image: '',
-        ),
-      ];
-      final position = Position(
-        latitude: -23.5505,
-        longitude: -46.6333,
-        timestamp: DateTime.now(),
-        accuracy: 0,
-        altitude: 0,
-        altitudeAccuracy: 0,
-        heading: 0,
-        headingAccuracy: 0,
-        speed: 0,
-        speedAccuracy: 0,
-      );
-
-      when(mockOffersRepository.fetchOffers())
-          .thenAnswer((_) async => Ok(offers));
-      when(mockLocationRepository.determinePosition())
-          .thenAnswer((_) async => Ok(position));
-
-      viewModel = OffersViewmodel(
-        offersRepository: mockOffersRepository,
-        locationRepository: mockLocationRepository,
-      );
-
-      await Future.delayed(Duration.zero); // allow async operations to complete
-
-      expect(viewModel.offers, isNotNull);
-      expect(viewModel.offers!.length, 1);
-      expect(viewModel.offers![0].discount, '20% OFF');
-      expect(viewModel.offers![0].distance, 0);
-      expect(viewModel.offers![0].distanceInMeters, '0m');
+    setUpAll(() {
+      provideDummy<Result<List<Offer>>>(_dummyResultOffer());
+      provideDummy<Result<Position>>(_dummyResultPosition());
     });
 
+    test(
+      'fetches offers and location, calculates distance and discount correctly',
+      () async {
+        when(
+          mockOffersRepository.fetchOffers(),
+        ).thenAnswer((_) async => Result.ok(offers));
+        when(
+          mockLocationRepository.determinePosition(),
+        ).thenAnswer((_) async => Result.ok(position));
+
+        viewModel = OffersViewmodel(
+          offersRepository: mockOffersRepository,
+          locationRepository: mockLocationRepository,
+        );
+
+        await Future.delayed(
+          Duration.zero,
+        ); // allow async operations to complete
+
+        expect(viewModel.offers, isNotNull);
+        expect(viewModel.offers!.length, 1);
+        expect(viewModel.offers![0].discount, '20% OFF');
+        expect(viewModel.offers![0].distance, 0);
+        expect(viewModel.offers![0].distanceInMeters, '0m');
+      },
+    );
+
     test('handles error when fetching offers', () async {
-      when(mockOffersRepository.fetchOffers())
-          .thenAnswer((_) async => Error(Exception('Failed to fetch offers')));
-      when(mockLocationRepository.determinePosition())
-          .thenAnswer((_) async => Ok(Position(
-                latitude: 0,
-                longitude: 0,
-                timestamp: DateTime.now(),
-                accuracy: 0,
-                altitude: 0,
-                altitudeAccuracy: 0,
-                heading: 0,
-                headingAccuracy: 0,
-                speed: 0,
-                speedAccuracy: 0,
-              )));
+      when(mockOffersRepository.fetchOffers()).thenAnswer(
+        (_) async => Result.error(Exception('Failed to fetch offers')),
+      );
+      when(mockLocationRepository.determinePosition()).thenAnswer(
+        (_) async => Result.ok(
+          Position(
+            latitude: 0,
+            longitude: 0,
+            timestamp: DateTime.now(),
+            accuracy: 0,
+            altitude: 0,
+            altitudeAccuracy: 0,
+            heading: 0,
+            headingAccuracy: 0,
+            speed: 0,
+            speedAccuracy: 0,
+          ),
+        ),
+      );
 
       viewModel = OffersViewmodel(
         offersRepository: mockOffersRepository,
@@ -98,17 +121,20 @@ void main() {
     test('handles error when fetching location', () async {
       final offers = [
         Offer(
-            currentPrice: 80,
-            originalPrice: 100,
-            store: 'Store A',
-            product: 'Product A',
-            image: '')
+          currentPrice: 80,
+          originalPrice: 100,
+          store: 'Store A',
+          product: 'Product A',
+          image: '',
+        ),
       ];
 
-      when(mockOffersRepository.fetchOffers())
-          .thenAnswer((_) async => Ok(offers));
+      when(
+        mockOffersRepository.fetchOffers(),
+      ).thenAnswer((_) async => Result.ok(offers));
       when(mockLocationRepository.determinePosition()).thenAnswer(
-          (_) async => Error(Exception('Failed to fetch location')));
+        (_) async => Result.error(Exception('Failed to fetch location')),
+      );
 
       viewModel = OffersViewmodel(
         offersRepository: mockOffersRepository,
@@ -118,46 +144,6 @@ void main() {
       await Future.delayed(Duration.zero);
 
       expect(viewModel.offers, isNotNull);
-      expect(viewModel.offers![0].distance, 0);
-      expect(viewModel.offers![0].distanceInMeters, 'N/A');
-    });
-
-    test('handles offers without location data', () async {
-      final offers = [
-        Offer(
-            currentPrice: 80,
-            originalPrice: 100,
-            store: 'Store A',
-            product: 'Product A',
-            image: '')
-      ];
-      final position = Position(
-        latitude: -23.5505,
-        longitude: -46.6333,
-        timestamp: DateTime.now(),
-        accuracy: 0,
-        altitude: 0,
-        altitudeAccuracy: 0,
-        heading: 0,
-        headingAccuracy: 0,
-        speed: 0,
-        speedAccuracy: 0,
-      );
-
-      when(mockOffersRepository.fetchOffers())
-          .thenAnswer((_) async => Ok(offers));
-      when(mockLocationRepository.determinePosition())
-          .thenAnswer((_) async => Ok(position));
-
-      viewModel = OffersViewmodel(
-        offersRepository: mockOffersRepository,
-        locationRepository: mockLocationRepository,
-      );
-
-      await Future.delayed(Duration.zero);
-
-      expect(viewModel.offers, isNotNull);
-      expect(viewModel.offers!.length, 1);
       expect(viewModel.offers![0].distance, 0);
       expect(viewModel.offers![0].distanceInMeters, 'N/A');
     });
